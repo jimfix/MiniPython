67bc40b257811706afa2bc6c61a7cde5f401eff1
Jeremy Cosel <jmcosel@gmail.com>
2015-10-05 18:25:12 -0700

    minor cleanup, cup still needs work

--- src/TestLexer.java
+++ src/TestLexer.java
@@ -7,8 +7,8 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 import java.io.*;
-import java_cup.runtime.Symbol;
 
+import java_cup.runtime.Symbol;
 
 /**
  * Simple test driver for the java lexer. Just runs it on some
@@ -17,14 +17,7 @@ import java_cup.runtime.Symbol;
  */
 public class TestLexer {
 
-  /** some numerals to for lexer testing */
   int intDec = 37;
-  long longDec = 37l;
-  int intHex = 0x0001;
-  long longHex = 0xFFFFl;
-  int intOct = 0377;
-  long longOc = 007l;
-  int smallest = -2147483648;   
 
   public static void main(String argv[]) {
 
