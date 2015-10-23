310c50ae3a37df2779a1b7d525fc82db4cea68bb
Jeremy Cosel <jcosel@reed.edu>
2015-10-22 18:24:38 -0700

    More file removal & README update.

--- src/Literal.java
+++ src/Literal.java
@@ -1,3 +1,12 @@
-public abstract class Literal {
+public class Literal extends Expn {
 
+	int value;
+
+	public Literal(int v) {
+		this.value = v;
+	}
+
+	public String toString() {
+		return "INT_LIT("+this.value+")";
+	}
 }
