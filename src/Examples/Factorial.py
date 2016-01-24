5553601bedede226a997d97ddd97f4f00474f3a5
Jeremy Cosel <jcosel@reed.edu>
2016-01-24 13:22:24 -0800

    oops, overwrote Factorial code

--- src/Examples/Factorial.py
+++ src/Examples/Factorial.py
@@ -1,45 +1,6 @@
-print " Part 1: "
-
-def square(x):
-    return x*x
-print square(4)
-print square
-
-print " Part 2: "
-
-x = 5
-if x < 6:
-    print 1
-else:
-    print 0
-
-print " Part 3: "
-
-i = 0
-max = 5
-while i < max-1:
-    print i
-    i = i + 1
-
-print " Part 4: "
-
-def return6():
-    return 6
-x = 5
-print (x <= 5)
-print (x <= (return6()))
-print (x <= (return6()-2))
-print (10 <= (10-20))
-
-print " Part 5: "
-
-def looptest(x):
-    i = 0
-    sum = 0
-    while (i < x):
-        j = 0
-        while j < i:
-            print j
-            j = j+1
-        i=i+1
-looptest(4)
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+print factorial(5)
